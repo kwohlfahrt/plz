94ab0da08e7e7b7747664e524acb60a947a1a752
Kai Wohlfahrt <kai@prodo.ai>
2018-09-18 17:11:33 +0100

    Add nix script for package
    Currently paramiko is a pain - not upstreaming due to pytest_relaxed
    requirement being messy.

---
+++ cli/plz-cli.nix
@@ -0,0 +1,45 @@
+{ stdenv, python3, buildPythonPackage, buildPythonApplication, fetchPypi,
+  docker, prettytable, python-dateutil, requests, urllib3,
+  nose, flask }:
+
+let
+  glob2 = buildPythonPackage rec {
+    version = "0.6";
+    pname = "glob2";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "1miyz0pjyji4gqrzl04xsxcylk3h2v9fvi7hsg221y11zy3adc7m";
+    };
+  };
+
+  prettytable_0_7_2 = prettytable.overridePythonAttrs rec {
+    pname = "prettytable";
+    version = "0.7.2";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "1ndckiniasacfqcdafzs04plskrcigk7vxprr2y34jmpkpf60m1d";
+    };
+  };
+
+  paramiko_2_4_2 = python3.pkgs.callPackage ./paramiko.nix {};
+
+in buildPythonApplication rec {
+  name = "${pname}-${version}";
+  version = "0.1.0";
+  pname = "plz-cli";
+
+  propagatedBuildInputs = [
+    docker glob2 paramiko_2_4_2 prettytable_0_7_2 python-dateutil requests urllib3
+  ];
+
+  checkInputs = [ nose flask ];
+  doCheck = true;
+  checkPhase = ''
+    nosetests
+  '';
+
+  src = ../.;
+  sourceRoot = "plz/cli";
+}
