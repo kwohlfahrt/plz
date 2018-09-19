034340306e551b07903fab5b4c5a7ca5379f896c
Kai Wohlfahrt <kai@prodo.ai>
2018-09-19 18:03:46 +0100

    Allow installing a specific timestamp in nixexpr

--- cli/default.nix
+++ cli/default.nix
@@ -1,1 +1,3 @@
-{ nixpkgs ? import <nixpkgs> {} }: nixpkgs.python3Packages.callPackage ./plz-cli.nix {}
+{ nixpkgs ? import <nixpkgs> {}, timestamp ? null, sha256 ? null }:
+
+nixpkgs.python3Packages.callPackage ./plz-cli.nix { timestamp = timestamp; sha256 = sha256;}
--- cli/plz-cli.nix
+++ cli/plz-cli.nix
@@ -1,6 +1,6 @@
-{ stdenv, python3, buildPythonPackage, buildPythonApplication, fetchPypi,
+{ stdenv, python3, buildPythonPackage, buildPythonApplication, fetchPypi, requireFile,
   docker, prettytable, python-dateutil, requests, urllib3,
-  nose, flask }:
+  nose, flask,  timestamp ? null, sha256 ? null }:
 
 let
   glob2 = buildPythonPackage rec {
@@ -25,23 +25,40 @@ let
 
   paramiko_2_4_2 = python3.pkgs.callPackage ./paramiko.nix {};
 
-  timestamp = builtins.replaceStrings ["\n"] [""] (builtins.readFile ../STABLE_BUILD_TIMESTAMP);
+  default_timestamp = builtins.replaceStrings ["\n"] [""] (builtins.readFile ../STABLE_BUILD_TIMESTAMP);
+  version = "0.1.${if timestamp != null then (toString timestamp) else default_timestamp}";
+  hash = sha256; # Avoid infinite recusion
 
-in buildPythonApplication rec {
+in buildPythonApplication (rec {
+  inherit version;
   name = "${pname}-${version}";
-  version = "0.1.${timestamp}";
   pname = "plz-cli";
 
   propagatedBuildInputs = [
     docker glob2 paramiko_2_4_2 prettytable_0_7_2 python-dateutil requests urllib3
   ];
+} // (if timestamp == null then {
+  src = ../.;
+  sourceRoot = "plz/cli";
 
   checkInputs = [ nose flask ];
   doCheck = true;
   checkPhase = ''
     nosetests
   '';
+} else {
+  format = "wheel";
 
-  src = ../.;
-  sourceRoot = "plz/cli";
-}
+  src = requireFile rec {
+    name = "plz_cli-${version}-py3-none-any.whl";
+    sha256 = if hash != null then hash else abort message;
+
+    message = ''
+      When installing a specific timestamp, ${name} must be prefetched. Run:
+
+          nix-prefetch-url "https://s3-eu-west-1.amazonaws.com/plz.prodo.ai/${name}"
+
+      and then re-run the derivation with the `sha256` argument.
+    '';
+  };
+}))
