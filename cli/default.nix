bd7db66116760f4bb94565a15917bae5dc96d356
Kai Wohlfahrt <kai@prodo.ai>
2018-12-17 18:09:57 +0000

    Use overlay for out-of-tree packages

--- cli/default.nix
+++ cli/default.nix
@@ -2,30 +2,18 @@
 
 let
   overlay = self: super: {
-    python3 = super.python3.override {
-      packageOverrides = pyself: pysuper: {
-        requests = pysuper.requests.overridePythonAttrs (pyold: rec {
-          version = "2.21.0";
+    pythonOverrides = super.lib.composeExtensions super.pythonOverrides (pyself: pysuper: {
+      requests = pysuper.requests.overridePythonAttrs (pyold: rec {
+        version = "2.21.0";
 
-          src = pyold.src.override {
-            inherit version;
-            sha256 = "13jr0wkj9c2j8c0c8iaal9iivi0bpxghnsdn6lxcpnmc657q4ajh";
-          };
-        });
-
-        glob2 = pyself.buildPythonPackage rec {
-          version = "0.6";
-          pname = "glob2";
-
-          src = pyself.fetchPypi {
-            inherit pname version;
-            sha256 = "1miyz0pjyji4gqrzl04xsxcylk3h2v9fvi7hsg221y11zy3adc7m";
-          };
+        src = pyold.src.override {
+          inherit version;
+          sha256 = "13jr0wkj9c2j8c0c8iaal9iivi0bpxghnsdn6lxcpnmc657q4ajh";
         };
-      };
-    };
+      });
+    });
   };
 
-  pkgs = import <nixpkgs> { overlays = [overlay]; };
+  pkgs = import <nixpkgs> { overlays = (import ~/.config/nixpkgs/overlays.nix) ++ [overlay]; };
 
 in pkgs.python3.pkgs.callPackage ./plz-cli.nix { timestamp = timestamp; sha256 = sha256; }
